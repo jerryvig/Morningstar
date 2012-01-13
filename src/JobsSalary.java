3d01f5944e962b258fc0676533fe233ef80b3a2f
Jerry Vigil <jerry.vigil@mktneutral.com>
2012-01-13 07:30:56 -0500

    commit to include the JobsSalary.java code and data.

---
+++ src/JobsSalary.java
@@ -0,0 +1,76 @@
+import au.com.bytecode.opencsv.CSVReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.NoSuchElementException;
+
+public class JobsSalary {
+ public static void main( String[] args ) {
+   try {
+     CSVReader reader = new CSVReader( new FileReader("/root/Desktop/Morningstar/morningstarFact.csv" ) );
+     BufferedWriter writer = new BufferedWriter( new FileWriter("/root/Desktop/Morningstar/JobsSalaryData.csv") );
+     writer.write( "\"Ticker Symbol\",\"Company Name\",\"City\",\"State\",\"Salary Average\",\"Salary Range\"\n" );
+
+     String[] nextLine;
+     HtmlUnitDriver driver = new HtmlUnitDriver( BrowserVersion.FIREFOX_3_6 );
+     driver.setJavascriptEnabled( false );
+  
+     int rowCount = 0;
+     while ( (nextLine = reader.readNext()) != null ) {
+      if ( rowCount > 0 ) {      
+       String ticker = nextLine[0].trim();
+       String companyName = nextLine[15].trim();
+       String city = nextLine[25].trim();
+       String state = nextLine[26].trim();
+       String sAverage = "";
+       String sRange = "";      
+ 
+       companyName = companyName.toLowerCase().trim();
+       companyName = companyName.replace(".","");
+       companyName = companyName.replace("!","");
+       companyName = companyName.replaceAll("corporation","");
+       companyName = companyName.replaceAll("incorporated","");
+       companyName = companyName.replaceAll(",","");
+       companyName = companyName.replaceAll("ltd","");
+       companyName = companyName.replaceAll("inc","");
+       companyName = companyName.replaceAll("ltd","");
+       companyName = companyName.replaceAll("corp","");
+       companyName = companyName.replaceAll("lp","");
+       companyName = companyName.replaceAll("sa","");
+       companyName = companyName.trim();
+       companyName = companyName.replaceAll(" ","+");
+       city = city.replaceAll(" ","+").trim();
+
+       String baseUrl = "http://www.jobs-salary.com/salaries.php?q=";
+       String fetchUrl = baseUrl + companyName + "&lc=" + city + "+" + state;        
+       System.out.println( fetchUrl );
+
+       driver.get( fetchUrl );     
+       try {
+         WebElement srangeDiv = driver.findElementByXPath( "//div[@class='srange']" );
+         String srangeText = srangeDiv.getText();
+         String[] pieces = srangeText.split(":");
+         sAverage = pieces[1].trim().replace("Salary Range","");
+         sRange = pieces[2].trim();
+         sRange = sRange.replaceAll("\n"," ");
+       } catch ( NoSuchElementException nsee ) { nsee.printStackTrace(); }
+ 
+       try {
+         writer.write( "\"" + ticker + "\",\"" + companyName + "\",\"" + city + "\",\"" + state + "\",\"" + sAverage + "\",\"" + sRange + "\"\n" );
+         System.out.print( "\"" + ticker + "\",\"" + companyName + "\",\"" + city + "\",\"" + state + "\",\"" + sAverage + "\",\"" + sRange + "\"\n" );
+       } catch ( IOException ioe ) { ioe.printStackTrace(); }
+
+       try { Thread.sleep( 400 ); } catch ( InterruptedException ie ) { ie.printStackTrace(); }     
+      }
+      rowCount++;
+     }
+
+     reader.close();
+     writer.close();
+   }catch ( IOException ioe ) { ioe.printStackTrace(); }
+ }
+}
