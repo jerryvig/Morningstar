54091d2069decb53f4616ec765c1b69f5dcba465
Jerry Vigil <jerry.vigil@mktneutral.com>
2012-09-16 19:11:40 -0700

    7:11 pm commit

--- node/compete.sql
+++ node/compete.sql
@@ -1,17 +1,21 @@
 SELECT load_extension('./libsqlitefunctions.so');
 
-CREATE TEMPORARY TABLE ordered_data ( domain TEXT, year_month TEXT, unique_visitors INTEGER );
+DROP TABLE IF EXISTS ordered_data;
+DROP TABLE IF EXISTS visitor_growth;
+DROP TABLE IF EXISTS sharpe_growth;
+
+CREATE TABLE ordered_data ( domain TEXT, year_month TEXT, unique_visitors INTEGER );
 
 INSERT INTO ordered_data SELECT domain, year_month, unique_visitors FROM compete_data ORDER BY domain ASC, year_month ASC;
 
-CREATE TEMPORARY TABLE visitor_growth ( domain TEXT, year_month TEXT, unique_visitors INTEGER, visitor_growth REAL );
+CREATE TABLE visitor_growth ( domain TEXT, year_month TEXT, unique_visitors INTEGER, visitor_growth REAL );
 
 INSERT INTO visitor_growth SELECT t1.domain, t1.year_month, t1.unique_visitors, CAST((t1.unique_visitors-t2.unique_visitors) AS REAL)/CAST(t2.unique_visitors AS REAL) FROM ordered_data AS t1 INNER JOIN ordered_data AS t2 ON (t2.domain=t1.domain AND t2.rowid=(t1.rowid-1)) ORDER BY t1.rowid ASC;
 
-CREATE TEMPORARY TABLE cumulative_visitor_growth ( domain TEXT, cum_growth REAL );
+--CREATE TEMPORARY TABLE cumulative_visitor_growth ( domain TEXT, cum_growth REAL );
 
-INSERT INTO cumulative_visitor_growth SELECT domain, EXP(SUM(LOG(1.0+visitor_growth)))-1.0 FROM visitor_growth WHERE visitor_growth > -1.0 GROUP by domain;
+--INSERT INTO cumulative_visitor_growth SELECT domain, EXP(SUM(LOG(1.0+visitor_growth)))-1.0 FROM visitor_growth WHERE visitor_growth > -1.0 GROUP by domain;
 
-CREATE TEMPORARY TABLE sharpe_growth ( domain TEXT, max_unique_visitors, sharpe_growth REAL );
+CREATE TABLE sharpe_growth ( domain TEXT, max_unique_visitors, cum_growth REAL, sharpe_growth REAL );
 
-INSERT INTO sharpe_growth SELECT domain, MAX(unique_visitors), AVG(visitor_growth)/STDEV(visitor_growth) FROM visitor_growth GROUP BY domain;
+INSERT INTO sharpe_growth SELECT domain, MAX(unique_visitors), EXP(SUM(LOG(1.0+visitor_growth)))-1.0, AVG(visitor_growth)/STDEV(visitor_growth) FROM visitor_growth WHERE visitor_growth>-1.0 GROUP BY domain;
---
+++ node/webApp.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var sqlite3 = require('sqlite3').verbose();
+var fs = require('fs');
+var PORT = 80;
+
+app.get('/',function(req, res) {
+   res.set('Content-Type','text/html');
+   fs.readFile('./competeData.html',function(err,data){
+       res.send( data );
+   });  
+});
+
+app.get('/compete_sharpe_data',function(req, res) {
+   var db = new sqlite3.Database('./compete_data.sqlite');
+   res.set('Content-Type', 'application/json');
+   var jsonResp = new Object();
+
+   db.all('SELECT * FROM sharpe_growth ORDER BY sharpe_growth DESC',function(err,rows){
+       if ( err ) throw err;
+       jsonResp.rows = rows;
+       res.json( jsonResp );
+       db.close();
+   });
+});
+
+app.listen( PORT );
+console.log('app listening on port '+PORT);
